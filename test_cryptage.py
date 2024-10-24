5bcf83f75f223cfe2b6b8143cdd4c43206d6b34b
Ri0-sama <sospaolomario@gmail.com>
2024-10-24 11:26:44 +0200

    premier test aui fail car nous n'avons pas implementer la fonction crypt

---
+++ test_cryptage.py
@@ -0,0 +1,12 @@
+import unittest
+from cryptage import Cryptage
+
+class TestCryptage(unittest.TestCase):
+    def setUp(self):
+        self.instance = Cryptage()
+
+    def test_crypt_message(self):
+        self.assertEqual(self.instance.crypt("abc"), "bcd")  # Test échouera ici
+
+if __name__ == "__main__":
+    unittest.main()
