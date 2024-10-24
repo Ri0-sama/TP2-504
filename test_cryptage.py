c09994ca074417d1c21245541c503ed145290ac4
Ri0-sama <sospaolomario@gmail.com>
2024-10-24 12:04:04 +0200

    test de decalage qui fail car la fonction ne prend pas encore le parametre de pas

--- test_cryptage.py
+++ test_cryptage.py
@@ -5,8 +5,10 @@ class TestCryptage(unittest.TestCase):
     def setUp(self):
         self.instance = Cryptage()
 
-    def test_crypt_message(self):
-        self.assertEqual(self.instance.crypt("abc"), "bcd") 
+    
+
+    def test_crypt_message_with_shift(self):
+        self.assertEqual(self.instance.crypt("abc", 2), "cde")  # Test échouera ici
 
 if __name__ == "__main__":
     unittest.main()
