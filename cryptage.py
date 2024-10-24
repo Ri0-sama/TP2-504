e51451737203ddff3599a707309a9566643cdf39
Ri0-sama <sospaolomario@gmail.com>
2024-10-24 13:35:58 +0200

    implementation de la fonction decrypt

--- cryptage.py
+++ cryptage.py
@@ -1,9 +1,32 @@
 import string
 
 class Cryptage:
+
+#Fonction de cryptage    
     def crypt(self, message, pas=1):
         caracteres = string.ascii_letters + string.digits + string.printable
         result = ""
         for char in message:
-            result += chr(ord(char) + pas)  # parametre de pas ajouté
+            if char in caracteres:
+                index = caracteres.index(char)
+               
+                new_index = (index + pas) % len(caracteres)
+                result += caracteres[new_index]
+            else:
+                result += char 
+        return result
+
+
+# Fonction de decryptage
+    def decrypt(self, message, pas=1):
+        caracteres = string.ascii_letters + string.digits + string.printable
+        result = ""
+        for char in message:
+            if char in caracteres:
+                index = caracteres.index(char)
+                
+                new_index = (index - pas) % len(caracteres)
+                result += caracteres[new_index]
+            else:
+                result += char  
         return result
